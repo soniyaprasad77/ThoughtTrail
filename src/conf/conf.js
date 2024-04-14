50c2db48a1084d8a8c11edf502a95128627d9f5d
Your Name <soniyagupta82270@gmail.com>
2024-04-14 13:01:03 +0530

    minor changes

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -6,3 +6,5 @@ const conf = {
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_BUCKET_ID)
 }
+
+export default conf;
