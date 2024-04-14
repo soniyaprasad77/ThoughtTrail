5263aaae042e19f48ea7f59fc531ec7039cc3a8d
Your Name <soniyagupta82270@gmail.com>
2024-04-14 13:23:12 +0530

    build authentication service with appwrite

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,7 +1,57 @@
-import {Client, Account, ID} from 'appwrite';
-
+import { Client, Account, ID } from "appwrite";
+import conf from "../conf/conf.js";
 export class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteUrl)
+      .setProject(conf.appwriteProjectId);
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name
+      );
+      if (userAccount) {
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (e) {
+      throw e;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (e) {
+      throw e;
+    }
+  }
 
+  async getCurrentUser() {
+    try {
+      return await this.account.get();
+    } catch (e) {
+      console.log("Appwrite service :: getCurrentUser :: error :: ", e);
+      throw e;
+    }
+    return null;
+  }
+
+  async logout() {
+    try {
+      return await this.account.deleteSession("current");
+    } catch (e) {
+        console.log("Appwrite service :: logout :: error :: ", e);
+      throw e;
+    }
+  }
 }
 
 const authService = new AuthService();
