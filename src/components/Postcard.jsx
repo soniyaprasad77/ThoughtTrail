6b4cc1f2e5d62d1fc099552bd0a595c36d0d55b6
Your Name <soniyagupta82270@gmail.com>
2024-04-15 18:23:11 +0530

    feat : add login and singup components using REACT HOOK FORM

--- src/components/Postcard.jsx
+++ src/components/Postcard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import appwriteService from "../services/appwrite";
+import appwriteService from "../appwrite/config.js";
 import { Link } from "react-router-dom";
 const Postcard = ({ $id, title, featuredImage }) => {
   return (
@@ -7,9 +7,9 @@ const Postcard = ({ $id, title, featuredImage }) => {
       <div className="w-full bg-gray-100 rounded-xl p-4">
         <div className="w-full justify-center mb-4">
           <img
-            src={appwriteService.getFilepreview(featuredImage)}
+            src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
-            className="rounded-lg"
+            className="rounded-xl"
           />
         </div>
         <h2 className="text-xl font-bold">{title}</h2>
